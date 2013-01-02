b0d1e9a395285e0ab4fb7ba7e12197ac50eb23da
tribon1980 <cch@yonyou.com>
2013-01-02 23:20:56 +0800

    first post

---
+++ login_files/axurerp_pagespecificscript.js
@@ -0,0 +1,114 @@
+﻿
+var PageName = 'login';
+var PageId = 'c3db9034ef3745969c14fa8464fbddbf'
+var PageUrl = 'login.html'
+document.title = 'login';
+var PageNotes = 
+{
+"pageName":"login",
+"showNotesNames":"False"}
+var $OnLoadVariable = '';
+
+var $CSUM;
+
+var hasQuery = false;
+var query = window.location.hash.substring(1);
+if (query.length > 0) hasQuery = true;
+var vars = query.split("&");
+for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0].length > 0) eval("$" + pair[0] + " = decodeURIComponent(pair[1]);");
+} 
+
+if (hasQuery && $CSUM != 1) {
+alert('Prototype Warning: The variable values were too long to pass to this page.\nIf you are using IE, using Firefox will support more data.');
+}
+
+function GetQuerystring() {
+    return '#OnLoadVariable=' + encodeURIComponent($OnLoadVariable) + '&CSUM=1';
+}
+
+function PopulateVariables(value) {
+    var d = new Date();
+  value = value.replace(/\[\[OnLoadVariable\]\]/g, $OnLoadVariable);
+  value = value.replace(/\[\[PageName\]\]/g, PageName);
+  value = value.replace(/\[\[GenDay\]\]/g, '5');
+  value = value.replace(/\[\[GenMonth\]\]/g, '9');
+  value = value.replace(/\[\[GenMonthName\]\]/g, '九月');
+  value = value.replace(/\[\[GenDayOfWeek\]\]/g, '星期三');
+  value = value.replace(/\[\[GenYear\]\]/g, '2012');
+  value = value.replace(/\[\[Day\]\]/g, d.getDate());
+  value = value.replace(/\[\[Month\]\]/g, d.getMonth() + 1);
+  value = value.replace(/\[\[MonthName\]\]/g, GetMonthString(d.getMonth()));
+  value = value.replace(/\[\[DayOfWeek\]\]/g, GetDayString(d.getDay()));
+  value = value.replace(/\[\[Year\]\]/g, d.getFullYear());
+  return value;
+}
+
+function OnLoad(e) {
+
+if (true) {
+
+}
+
+}
+
+var u3 = document.getElementById('u3');
+
+var u16 = document.getElementById('u16');
+
+u16.style.cursor = 'pointer';
+if (bIE) u16.attachEvent("onclick", Clicku16);
+else u16.addEventListener("click", Clicku16, true);
+function Clicku16(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="home-old.html" + GetQuerystring();
+
+}
+
+}
+
+var u12 = document.getElementById('u12');
+gv_vAlignTable['u12'] = 'top';
+var u15 = document.getElementById('u15');
+
+var u4 = document.getElementById('u4');
+gv_vAlignTable['u4'] = 'center';
+var u0 = document.getElementById('u0');
+
+var u8 = document.getElementById('u8');
+gv_vAlignTable['u8'] = 'top';
+var u19 = document.getElementById('u19');
+gv_vAlignTable['u19'] = 'top';
+var u10 = document.getElementById('u10');
+
+var u17 = document.getElementById('u17');
+gv_vAlignTable['u17'] = 'center';
+var u5 = document.getElementById('u5');
+gv_vAlignTable['u5'] = 'top';
+var u1 = document.getElementById('u1');
+gv_vAlignTable['u1'] = 'center';
+var u9 = document.getElementById('u9');
+gv_vAlignTable['u9'] = 'top';
+var u14 = document.getElementById('u14');
+
+var u20 = document.getElementById('u20');
+gv_vAlignTable['u20'] = 'top';
+var u6 = document.getElementById('u6');
+gv_vAlignTable['u6'] = 'top';
+var u2 = document.getElementById('u2');
+gv_vAlignTable['u2'] = 'top';
+var u11 = document.getElementById('u11');
+
+var u13 = document.getElementById('u13');
+gv_vAlignTable['u13'] = 'top';
+var u18 = document.getElementById('u18');
+gv_vAlignTable['u18'] = 'top';
+var u7 = document.getElementById('u7');
+
+if (window.OnLoad) OnLoad();
